72bec108ee459e0538a367b32f1e5d3642a01293
AJAJmon <jamon.wic@gmail.com>
2024-05-14 02:34:45 +0700

    update logic test_02

--- test_02/test_02.go
+++ test_02/test_02.go
@@ -4,53 +4,78 @@ import (
 	"fmt"
 )
 
-func processString(input string) {
+func processString(input string, num []int, index int) int {
+	char := input[index]
+	switch char {
+	case 'L':
+		if num[index] <= num[index+1] {
+			num[index]++
+		}
+	case 'R':
+		if num[index] >= num[index+1] {
+			num[index+1]++
+		}
+	}
+	return checkAnsNum(input, num)
+}
+
+func checkAnsNum(input string, num []int) int {
+	countTrue := 0
 	inputLength := len(input)
-	num := make([]int, len(input)+1)
-	left, right := true, true
-	for left || right {
+	for i := 0; i < len(input); i++ {
+		char := input[i]
+		switch char {
+		case 'L':
+			if num[i] > num[i+1] {
+				countTrue++
+			} else {
+				return processString(input, num, i)
+			}
+		case 'R':
+			if num[i] < num[i+1] {
+				countTrue++
+			} else {
+				return processString(input, num, i)
+			}
+		}
+	}
+
+	// Check how many equal
+	numOfEqual := 0
+	for _, equal := range input {
+		if equal == '=' {
+			numOfEqual++
+		}
+	}
+
+	// When codition is true
+	if countTrue == inputLength-numOfEqual {
+		// Check case '='
 		for i := 0; i < inputLength; i++ {
 			char := input[i]
 			switch char {
-			case 'L':
-				if num[i] > num[i+1] {
-					left = false
+			case '=':
+				if i != inputLength-1 {
+					num[i] = num[i+1]
 				} else {
-					num[i]++
-				}
-			case 'R':
-				if num[i] < num[i+1] {
-					right = false
-				} else {
-					num[i+1]++
+					num[i+1] = num[i]
 				}
 			}
 		}
-	}
-	// Check case '='
-	for i := 0; i < inputLength; i++ {
-		char := input[i]
-		switch char {
-		case '=':
-			if i != inputLength-1 {
-				num[i] = num[i+1]
-			} else {
-				num[i+1] = num[i]
-			}
+		// Output
+		for _, ansNum := range num {
+			fmt.Print(ansNum)
 		}
+		return 1
 	}
-	// Output
-	for _, ansNum := range num {
-		fmt.Print(ansNum)
-	}
+	return 0
 }
 
 func main() {
 	var input string
 	fmt.Scanln(&input)
-
-	// Process the input string
-	processString(input)
+	num := make([]int, len(input)+1)
+	processString(input, num, 0)
 }
 
 //	Test case
@@ -59,10 +84,18 @@ func main() {
 	==RLL
 	=LLRR
 	RRL=R
+	LLLRR
+	LLLLL
+	RRRRR
+	=====
 */
 /*
 	210122
 	000210
 	221012
 	012001
+	321012
+	543210
+	012345
+	000000
 */
